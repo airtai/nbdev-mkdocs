60cd2672a369fe4338adfdea76d7ea1854f221a0
Sean Bartik <9471675-seanbartik@users.noreply.gitlab.com>
2022-11-28 07:22:57 +0530

    Wip: Debug CI failure

--- nbdev_mkdocs/mkdocs.py
+++ nbdev_mkdocs/mkdocs.py
@@ -688,22 +688,22 @@ def prepare(root_path: str, no_test: bool = False, **kwargs):
             nbdev_prepare.__wrapped__()
 
         n_workers = multiprocessing.cpu_count()
-        nbs_path = _get_value_from_config(root_path, "nbs_path")
+        #     nbs_path = _get_value_from_config(root_path, "nbs_path")
 
         cache, cfg, path = _pre_docs(n_workers=n_workers, **kwargs)
 
-        # copy cname if it exists
-        copy_cname_if_needed(root_path)
+    # copy cname if it exists
+    copy_cname_if_needed(root_path)
 
-        # get lib name from settings.ini
-        lib_name = _get_value_from_config(root_path, "lib_name")
-        lib_path = _get_value_from_config(root_path, "lib_path")
+    # get lib name from settings.ini
+    lib_name = _get_value_from_config(root_path, "lib_name")
+    lib_path = _get_value_from_config(root_path, "lib_path")
 
-        build_summary(root_path, lib_path)
+    build_summary(root_path, lib_path)
 
-        cmd = f"mkdocs build -f \"{(Path(root_path) / 'mkdocs' / 'mkdocs.yml').resolve()}\""
-        print(f"Running cmd={cmd}")
-        _sprun(cmd)
+    cmd = f"mkdocs build -f \"{(Path(root_path) / 'mkdocs' / 'mkdocs.yml').resolve()}\""
+    print(f"Running cmd={cmd}")
+    _sprun(cmd)
 
 
 @call_parse
