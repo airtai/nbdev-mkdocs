81856a7a1129a832602e33f6c80cf2babc7e6051
Sean Bartik <9471675-seanbartik@users.noreply.gitlab.com>
2022-11-28 10:59:51 +0530

    Wip: Debug CI failure

--- nbdev_mkdocs/mkdocs.py
+++ nbdev_mkdocs/mkdocs.py
@@ -680,12 +680,20 @@ def prepare(root_path: str, no_test: bool = False, **kwargs):
             refresh_quarto_yml()
             nbdev_readme.__wrapped__(chk_time=True)
         else:
-            nbdev_prepare.__wrapped__()
+            #             nbdev_prepare.__wrapped__()
+            import nbdev.test, nbdev.clean
 
-        n_workers = multiprocessing.cpu_count()
-        nbs_path = _get_value_from_config(root_path, "nbs_path")
+            nbdev_export.__wrapped__()
+            # nbdev.test.nbdev_test.__wrapped__()
+            nbdev.clean.nbdev_clean.__wrapped__()
+            refresh_quarto_yml()
+            nbdev_readme.__wrapped__(chk_time=True)
 
-        cache, cfg, path = _pre_docs(n_workers=n_workers, **kwargs)
+
+#         n_workers = multiprocessing.cpu_count()
+#         nbs_path = _get_value_from_config(root_path, "nbs_path")
+
+#         cache,cfg,path = _pre_docs(n_workers=n_workers, **kwargs)
 
 
 #         # copy cname if it exists
