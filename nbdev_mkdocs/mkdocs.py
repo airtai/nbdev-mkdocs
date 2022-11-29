f8e9551df46000b16887864ceaafcd8cc4096fec
Sean Bartik <9471675-seanbartik@users.noreply.gitlab.com>
2022-11-29 19:09:55 +0530

    Wip: Debug CI failure

--- nbdev_mkdocs/mkdocs.py
+++ nbdev_mkdocs/mkdocs.py
@@ -279,8 +279,9 @@ def set_cwd(cwd_path: Union[Path, str]):
         os.chdir(original_cwd)
 
 # %% ../nbs/Mkdocs.ipynb 30
-@delegates(_nbglob_docs)
-def new(root_path: str, **kwargs):
+# @delegates(_nbglob_docs)
+# def new(root_path: str, **kwargs):
+def new(root_path: str):
     """Initialize mkdocs project files
 
     Creates **mkdocs** directory in the **root_path** directory and populates
@@ -322,7 +323,7 @@ def _sprun(cmd):
             f"CMD Failed: e={e}\n e.returncode={e.returncode}\n e.output={e.output}\n e.stderr={e.stderr}\n cmd={cmd}"
         )
 
-# %% ../nbs/Mkdocs.ipynb 36
+# %% ../nbs/Mkdocs.ipynb 37
 def _get_nbs_for_markdown_conversion(cache: Path):
     """Get a list of notebooks that needs to be converted to markdown.
 
@@ -331,7 +332,7 @@ def _get_nbs_for_markdown_conversion(cache: Path):
     """
     return list(cache.glob("index.ipynb")) + list(cache.glob("./guides/*.ipynb"))
 
-# %% ../nbs/Mkdocs.ipynb 38
+# %% ../nbs/Mkdocs.ipynb 39
 def _generate_markdown_from_nbs(root_path: str):
 
     doc_path = Path(root_path) / "mkdocs" / "docs"
@@ -357,7 +358,7 @@ def _generate_markdown_from_nbs(root_path: str):
             src_md = cache / "_docs" / f"{nb.stem}.md"
             shutil.move(src_md, dst_md)
 
-# %% ../nbs/Mkdocs.ipynb 40
+# %% ../nbs/Mkdocs.ipynb 41
 def _replace_all(text: str, dir_prefix: str) -> str:
     """Replace the images relative path in the markdown text
 
@@ -385,7 +386,7 @@ def _replace_all(text: str, dir_prefix: str) -> str:
 
     return text
 
-# %% ../nbs/Mkdocs.ipynb 42
+# %% ../nbs/Mkdocs.ipynb 43
 def _update_path_in_markdown(cache: Path, doc_path: Path):
     """Update guide images relative path in the markdown files
 
@@ -443,7 +444,7 @@ def _copy_guide_images_to_docs_dir(root_path: str):
 
         _update_path_in_markdown(cache, doc_path)
 
-# %% ../nbs/Mkdocs.ipynb 46
+# %% ../nbs/Mkdocs.ipynb 47
 def _get_title_from_notebook(nb_name: str) -> str:
     cache = proc_nbs()
     nb_path = Path(cache) / "guides" / f"{nb_name}.ipynb"
@@ -460,7 +461,7 @@ def _get_title_from_notebook(nb_name: str) -> str:
     nbp.process()
     return nbp.nb.frontmatter_["title"]
 
-# %% ../nbs/Mkdocs.ipynb 48
+# %% ../nbs/Mkdocs.ipynb 49
 def _generate_summary_for_guides(root_path: str) -> str:
     doc_path = Path(root_path) / "mkdocs" / "docs"
     mds = sorted(
@@ -478,7 +479,7 @@ def _generate_summary_for_guides(root_path: str) -> str:
     else:
         return ""
 
-# %% ../nbs/Mkdocs.ipynb 51
+# %% ../nbs/Mkdocs.ipynb 52
 def get_submodules(package_name: str) -> List[str]:
     # nosemgrep: python.lang.security.audit.non-literal-import.non-literal-import
     m = importlib.import_module(package_name)
@@ -493,7 +494,7 @@ def get_submodules(package_name: str) -> List[str]:
     ]
     return submodules
 
-# %% ../nbs/Mkdocs.ipynb 53
+# %% ../nbs/Mkdocs.ipynb 54
 def generate_api_doc_for_submodule(root_path: str, submodule: str) -> str:
     subpath = "API/" + submodule.replace(".", "/") + ".md"
     path = Path(root_path) / "mkdocs" / "docs" / subpath
@@ -510,15 +511,20 @@ def generate_api_doc_for_submodule(root_path: str, submodule: str) -> str:
 def generate_api_docs_for_module(root_path: str, module_name: str) -> str:
     submodules = get_submodules(module_name)
     shutil.rmtree(Path(root_path) / "mkdocs" / "docs" / "API", ignore_errors=True)
+
+    if not len(submodules):
+        return ""
+
     submodule_summary = "\n".join(
         [
             generate_api_doc_for_submodule(root_path=root_path, submodule=x)
             for x in submodules
         ]
     )
+
     return "- API\n" + textwrap.indent(submodule_summary, prefix=" " * 4)
 
-# %% ../nbs/Mkdocs.ipynb 55
+# %% ../nbs/Mkdocs.ipynb 56
 def _restrict_line_length(s: str, width: int = 80) -> str:
     """Restrict the line length of the given string.
 
@@ -542,7 +548,7 @@ def _restrict_line_length(s: str, width: int = 80) -> str:
                 _s += "\n" + line + "\n" if line.endswith(":") else " " + line + "\n"
     return _s
 
-# %% ../nbs/Mkdocs.ipynb 57
+# %% ../nbs/Mkdocs.ipynb 58
 def generate_cli_doc_for_submodule(root_path: str, cmd: str) -> str:
 
     cli_app_name = cmd.split("=")[0]
@@ -598,7 +604,7 @@ def generate_cli_docs_for_module(root_path: str, module_name: str) -> str:
 
     return "- CLI\n" + textwrap.indent(submodule_summary, prefix=" " * 4)
 
-# %% ../nbs/Mkdocs.ipynb 59
+# %% ../nbs/Mkdocs.ipynb 60
 def _copy_change_log_if_exists(
     root_path: Union[Path, str], docs_path: Union[Path, str]
 ) -> str:
@@ -610,7 +616,7 @@ def _copy_change_log_if_exists(
         changelog = "- [Releases](CHANGELOG.md)"
     return changelog
 
-# %% ../nbs/Mkdocs.ipynb 62
+# %% ../nbs/Mkdocs.ipynb 63
 def build_summary(
     root_path: str,
     module: str,
@@ -654,7 +660,7 @@ def build_summary(
     with open(docs_path / "SUMMARY.md", mode="w") as f:
         f.write(summary)
 
-# %% ../nbs/Mkdocs.ipynb 65
+# %% ../nbs/Mkdocs.ipynb 66
 def copy_cname_if_needed(root_path: str):
     cname_path = Path(root_path) / "CNAME"
     dst_path = Path(root_path) / "mkdocs" / "docs" / "CNAME"
@@ -669,9 +675,11 @@ def copy_cname_if_needed(root_path: str):
             f"File '{cname_path.resolve()}' not found, skipping copying..",
         )
 
-# %% ../nbs/Mkdocs.ipynb 67
-@delegates(_nbglob_docs)
-def prepare(root_path: str, no_test: bool = False, **kwargs):
+# %% ../nbs/Mkdocs.ipynb 68
+# @delegates(_nbglob_docs)
+# def prepare(root_path: str, no_test: bool = False, **kwargs):
+def prepare(root_path: str, no_test: bool = False):
+
     """Prepares mkdocs for serving
 
     Params:
@@ -688,10 +696,10 @@ def prepare(root_path: str, no_test: bool = False, **kwargs):
             print(f"Running cmd={cmd}")
             _sprun(cmd)
 
-        n_workers = multiprocessing.cpu_count()
-        nbs_path = _get_value_from_config(root_path, "nbs_path")
+        #         n_workers = multiprocessing.cpu_count()
+        #         nbs_path = _get_value_from_config(root_path, "nbs_path")
 
-        cache, cfg, path = _pre_docs(n_workers=n_workers, **kwargs)
+        #         cache,cfg,path = _pre_docs(n_workers=n_workers, **kwargs)
 
         # copy cname if it exists
         copy_cname_if_needed(root_path)
@@ -712,7 +720,7 @@ def prepare_cli(root_path: str):
     """Prepares mkdocs for serving"""
     prepare(root_path)
 
-# %% ../nbs/Mkdocs.ipynb 70
+# %% ../nbs/Mkdocs.ipynb 71
 def preview(root_path: str, port: Optional[int] = None):
     """Previes mkdocs documentation
 
