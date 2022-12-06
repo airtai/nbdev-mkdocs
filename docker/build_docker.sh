50ec4466fe9f78feb33fb1965124bf13588376c6
Tvrtko Sternak <117077296+Sternakt@users.noreply.github.com>
2022-12-06 14:13:19 +0100

    Implement build of minimal dev image for nbdev-mkdocs (#75)
    * Add Dockerfile for building the nbdev-mkdocs development container
    
    Add Dockerfile containing the minimum necessary dependencies for running the development
    environment, including:
    - Python, versions: 3.7, 3.8, 3.9 and 3.10
    - Jupyter notebook
    - Jupyter-black
    - Jupyter-themes (https://github.com/airtai/jupyter-themes.git)
    
    Add top_level_requirements.txt containing Python dependencies for running the development
    environment, including:
    - nbdev
    - nbdev-mkdos
    - twine
    - black
    
    Add Icon pack for Airt jupyter theme
    
    * Implement script for building and security check of dev container
    
    * Add LABEL in Dockerfile to reference nbdev-mkdocs repo
    
    * Implement initial github action for container build and deploy
    
    * Update build-dev-container.yml
    
    Change working directory to /docker
    
    * Update build-dev-container.yml
    
    * Update build-dev-container.yml
    
    Add trivy instalation step
    
    * Update build-dev-container.yml
    
    * Update build-dev-container.yml
    
    * Update build-dev-container.yml
    
    * Add unatended-updates to Dockerfile
    
    * Update build-dev-container.yml
    
    Remove feature branch from triggers for workflow (was included only for testing purposes)
    
    Co-authored-by: Tvrtko Sternak <tvrtko@airt.ai>

---
+++ docker/build_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if test -z "$CI_REGISTRY_IMAGE"
+then
+        export CI_REGISTRY_IMAGE=ghcr.io/airtai/nbdev-mkdocs
+fi
+
+if test -z "$CI_COMMIT_REF_NAME"
+then
+        export CI_COMMIT_REF_NAME=$(git branch --show-current)
+fi
+
+
+if [[ $CI_COMMIT_REF_NAME == "main" ]]; then TAG=latest ; else TAG=$CI_COMMIT_REF_NAME ; fi;
+
+
+echo Building $CI_REGISTRY_IMAGE, with tag: $TAG
+docker build -t $CI_REGISTRY_IMAGE:`date -u +%Y.%m.%d-%H.%M.%S` -t $CI_REGISTRY_IMAGE:$TAG .
+
+trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m -s CRITICAL,HIGH $CI_REGISTRY_IMAGE:$TAG
+# this one will fail if needed
+trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m --exit-code 1 --ignore-unfixed $CI_REGISTRY_IMAGE:$TAG
+
