1b7a0fd7bd8c04674e3ad99f2e3cf29a445eb1c0
Davor Runje <davor@airt.ai>
2022-12-10 23:13:43 +0000

    cuda docker image added

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -12,9 +12,23 @@ fi
 
 if [[ $CI_COMMIT_REF_NAME == "main" ]]; then TAG=latest ; else TAG=$CI_COMMIT_REF_NAME ; fi;
 
+export BASE=ubuntu:latest
+export PYTHON=3.10
 
 echo Building $CI_REGISTRY_IMAGE, with tag: $TAG
-docker build -t $CI_REGISTRY_IMAGE:`date -u +%Y.%m.%d-%H.%M.%S` -t $CI_REGISTRY_IMAGE:$TAG . \
+#docker build --build-arg BASE=$BASE --build-arg PYTHON=$PYTHON \
+#    -t $CI_REGISTRY_IMAGE:`date -u +%Y.%m.%d-%H.%M.%S` -t $CI_REGISTRY_IMAGE:$TAG . \
+#    && trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m -s CRITICAL,HIGH $CI_REGISTRY_IMAGE:$TAG \
+#    && trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m --exit-code 1 --ignore-unfixed $CI_REGISTRY_IMAGE:$TAG
+
+
+export CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE-cuda-11.2.1
+export BASE=nvidia/cuda:11.2.1-cudnn8-runtime-ubuntu20.04
+export PYTHON=3.9
+
+echo Building $CI_REGISTRY_IMAGE, with tag: $TAG
+docker build --build-arg BASE=$BASE --build-arg PYTHON=$PYTHON \
+    -t $CI_REGISTRY_IMAGE:`date -u +%Y.%m.%d-%H.%M.%S` -t $CI_REGISTRY_IMAGE:$TAG . \
     && trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m -s CRITICAL,HIGH $CI_REGISTRY_IMAGE:$TAG \
     && trivy image --skip-files /usr/local/bin/git-secrets --no-progress --timeout 10m --exit-code 1 --ignore-unfixed $CI_REGISTRY_IMAGE:$TAG
 
