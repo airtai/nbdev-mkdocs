448fe1b05d3557ad79db45658589a9c1cb9c4d29
Davor Runje <davor@airt.ai>
2022-12-11 15:50:38 +0100

    bug fix

--- docker/build_docker_cuda.sh
+++ docker/build_docker_cuda.sh
@@ -12,8 +12,9 @@ fi
 
 if [[ $CI_COMMIT_REF_NAME == "main" ]]; then TAG=latest ; else TAG=$CI_COMMIT_REF_NAME ; fi;
 
-export BASE=ubuntu:latest
-export PYTHON=3.10
+export CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE-cuda-11.2.1
+export BASE=nvidia/cuda:11.2.1-cudnn8-runtime-ubuntu20.04
+export PYTHON=3.9
 
 echo Building $CI_REGISTRY_IMAGE, with tag: $TAG
 docker build --build-arg BASE=$BASE --build-arg PYTHON=$PYTHON \
