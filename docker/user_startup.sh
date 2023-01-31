14cc5c08919f14df1ddddf3bff9264faac4de10a
Davor Runje <davor@airt.ai>
2023-01-31 09:32:21 +0100

    added gen-docstring button to Jupyter notebook in docker images (#140)

--- docker/user_startup.sh
+++ docker/user_startup.sh
@@ -2,5 +2,8 @@
 
 jt -t airtd -cellw 90% -N -T  --logo /tmp/airt-neg-trans-small.png --fav_icon_dir /tmp/airt_favicons
 
+jupyter nbextension install https://github.com/airtai/jupyter-docstring-gen/archive/main.zip --user
+
 jupyter nbextension enable jupyter-black-master/jupyter-black
+jupyter nbextension enable jupyter-docstring-gen-main/jupyter-docstring-gen
 
